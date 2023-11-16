83d778c0b30c506156eaa7744ccaf43c045b9999
Marco Frömbgen <23717573+mfroembgen@users.noreply.github.com>
2023-11-16 16:41:03 +0100

    fix: allow ipv6 inside security group

--- variables.tf
+++ variables.tf
@@ -90,27 +90,30 @@ variable "security_group_rules" {
   type        = any
   default = {
     ingress_all_http = {
-      type        = "ingress"
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      description = "HTTP web traffic"
-      cidr_blocks = ["0.0.0.0/0"]
+      type             = "ingress"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      description      = "HTTP web traffic"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
     }
     ingress_all_https = {
-      type        = "ingress"
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      description = "HTTPS web traffic"
-      cidr_blocks = ["0.0.0.0/0"]
+      type             = "ingress"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      description      = "HTTPS web traffic"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
     }
     egress_all = {
-      type        = "egress"
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+      type             = "egress"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
     }
   }
 }
