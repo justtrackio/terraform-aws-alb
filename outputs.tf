c1eddf2df263566dc8b7ef3f3cb19dffaeb6c133
Marco Frömbgen <23717573+mfroembgen@users.noreply.github.com>
2024-01-26 09:37:36 +0100

    fix: allow setting of listener rules

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,3 @@
-output "lb_dns_name" {
-  description = "The DNS name of the load balancer"
-  value       = try(module.alb.lb_dns_name, "")
-}
-
-output "lb_zone_id" {
-  description = "The zone_id of the load balancer to assist with creating DNS records"
-  value       = try(module.alb.lb_zone_id, "")
-}
-
 output "http_tcp_listener_arns" {
   description = "The ARN of the TCP and HTTP load balancer listeners created."
   value       = module.alb.http_tcp_listener_arns
@@ -16,4 +6,14 @@ output "http_tcp_listener_arns" {
 output "https_listener_arns" {
   description = "The ARNs of the HTTPS load balancer listeners created."
   value       = module.alb.https_listener_arns
+}
+
+output "lb_dns_name" {
+  description = "The DNS name of the load balancer"
+  value       = try(module.alb.lb_dns_name, "")
+}
+
+output "lb_zone_id" {
+  description = "The zone_id of the load balancer to assist with creating DNS records"
+  value       = try(module.alb.lb_zone_id, "")
 }
