cac239e80f497a0dfc4ec987b7cdff9f2fac6bd8
Sebastian Struß <struss@justtrack.io>
2024-04-12 14:45:26 +0200

    feat: allow to define glue label order

--- label.tf
+++ label.tf
@@ -5,3 +5,11 @@ module "alb_label" {
   context     = module.this.context
   label_order = var.label_orders.ec2
 }
+
+module "glue_label" {
+  source  = "justtrackio/label/null"
+  version = "0.26.0"
+
+  context     = module.this.context
+  label_order = var.label_orders.glue
+}
